38ccebff35a03ad4b7e46b46381bc22e3e78834c
Evgeniy Klivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2023-07-22 23:40:25 +0300

    Add files via upload

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -41,29 +41,9 @@
         @endforeach
       </ul>
       ---------------->    
-    <form action="{{route('register')}}" method="post"  autocomplete="off" >
+    <form action="{{route('login')}}" method="post"  autocomplete="off" >
         @csrf<!- буде показувати домен в браузері->
-          <div class="mb-6">
-            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-            <div class="relative mt-1 rounded-md shadow-sm">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-3">
-                <svg class="h-5 w-5 {{ $errors->has('name') ? 'text-red-400': 'text-gray-400' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-                </svg>
-              </div>
-              <input type="text" id="name" name="name" value="{{ old('name') }}" required autofocus class="w-full rounded-md  pl-10 text-sm  {{$errors->has('name') ? 'text-red-900 placeholder-red-300 focus:border-red-500 focus:ring-red-500 border-red-300' : 'border-gray-300 focus:border-green-500 focus:ring-green-500'}}" placeholder="Name" />
-              @error('name')
-              <div class="absolute inset-y-0 right-0 flex items-center pr-3">
-                <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
-                </svg>
-              </div>
-              @enderror
-            </div>
-            @error('name')
-            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-            @enderror
-          </div>
+          
 <!----------------------------------------------------Email----------------------------------------------------------------------------------------->
           <div class="mb-6">
             <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
@@ -105,34 +85,18 @@
               @enderror
             </div>
           </div>
+          <a href="{{ route('dashboard') }}" class="w-full flex items-center justify-center rounded-md bg-green-600 py-2 px-4 font-semibold text-white shadow-lg transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 
+            focus:ring-green-500 focus:ring-offset-2">Create</a>
+          </div>
 <!-----------------------------------------------Confirm Password------------------------------------------------------------------------------------------>
-          <div class="mb-6">
-            <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm Password</label>
-            <div class="relative mt-1 rounded-md shadow-sm">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-3">
-                <svg class="h-5 w-5 {{ $errors->has('password') ? 'text-red-400': 'text-gray-400'}}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                </svg>
-              </div>
-              <input type="password" id="password_confirmation" name="password_confirmation" minlength="8" class="w-full rounded-md pl-10 text-sm {{$errors->has('password_confirmation') ? 'text-red-900 placeholder-red-300 focus:border-red-500 focus:ring-red-500 border-red-300' : 'border-gray-300 focus:border-green-500 focus:ring-green-500'}}" placeholder="Minimum 8 characters"/>
-              @error('password_cofirmation')
-              <div class="absolute inset-y-0 right-0 flex items-center pr-3">
-                <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
-                </svg>
-              </div>
-              @enderror
-            </div>
-          </div>
-          <div>
-            <a href="{{ route('login') }}" class="w-full flex items-center justify-center rounded-md bg-green-600 py-2 px-4 font-semibold text-white shadow-lg transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 
-            focus:ring-green-500 focus:ring-offset-2">Register</a>
-          </div>
+          
+          
         </form>
-
+<!-------------------------------------
         <div class="mt-6 flex items-center justify-center">
           <a href="{{ route('dashboard') }}" class="text-sm font-medium text-green-600 hover:text-green-500">Already have an account?</a>
         </div>
+--------------------------------->
       </div>
     </main>
   </body>
